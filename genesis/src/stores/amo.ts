bef94e2c91a7af501eeb9a69ae70a7b2d88de1b4
Fn4ch <farhadmainsot@gmail.com>
2023-02-19 20:57:36 +0300

    + api for serv

--- genesis/src/stores/amo.ts
+++ genesis/src/stores/amo.ts
@@ -4,32 +4,20 @@ import { ref } from 'vue'
 
 export const useAmoApi = defineStore('amoApi', () => {
     const created = ref<any>([])
-    const token = ref<string>()
-
-    const getToken = () => {
-        axios.get<string>('https://test.gnzs.ru/oauth/get-token.php', {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                'X-Client-Id': '30878566'
-            }
-        }).then(r => token.value = r.toString())
-    }
 
     const createDeal = () => {
-        axios.post('https://www.amocrm.ru/api/v4/contacts').then(r => created.push(r))
+        axios.post('http://localhost:3000/api/createDeal').then(r => created.push(r))
     }
 
     const createContact = () => {
-        axios.post('https://www.amocrm.ru/api/v4/contacts').then(r => created.push(r))
+        axios.post('http://localhost:3000/api/createContact').then(r => created.push(r))
     }
 
     const createCompany = () => {
-        axios.post('https://www.amocrm.ru/api/v4/contacts').then(r => created.push(r))
+        axios.post('http://localhost:3000/api/createCompany').then(r => created.push(r))
     }
 
     return {
-        created, token, createCompany, createDeal, createContact, getToken
+        createCompany, createDeal, createContact
     }
 })
